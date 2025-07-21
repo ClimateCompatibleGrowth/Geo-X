5be125d5b9cba9741ffdc37b4518b34b4c4f2c89
samiyhanaqvi <samiyha.naqvi@tss.ox.ac.uk>
2025-07-21 12:10:51 -0600

    Updated epsg validation in transport_optimization.py

--- src/main/transport_optimization.py
+++ src/main/transport_optimization.py
@@ -90,7 +90,9 @@ def main():
     if plant_type == "hydrogen":
         conversion_params_filepath = f'parameters/{snakemake.wildcards.country}/{snakemake.wildcards.plant_type}/conversion_parameters.xlsx'
     hexagons = gpd.read_file(str(snakemake.input.hexagons))
-    if hexagons.crs is not None and hexagons.crs.to_epsg() != 4326:
+    if hexagons.crs is None:
+        hexagons = hexagons.set_crs(epsg=4326)
+    elif hexagons.crs.to_epsg() != 4326:
         hexagons = hexagons.to_crs(epsg=4326)
 
     infra_data = pd.read_excel(tech_params_filepath,
